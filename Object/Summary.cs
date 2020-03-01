58112218359b6d4b6c18bdafb66f4c24f9523b20
Tulesha <tulesha_xochet_pit@mail.ru>
2020-03-01 20:18:07 +0300

    first commit

---
+++ Object/Summary.cs
@@ -0,0 +1,9 @@
+namespace _2_Lab_Pairs
+{
+    class Sum
+    {
+        public static double Summator(double a, double b){
+            return a + b;
+        }
+    }   
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _2_Lab_Pairs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Sum.Summator(1.1, 3.9));
+        }
+    }
+}
